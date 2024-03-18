b9e120bd0afab6c59851f14686b6a963129f5094
tb0902 <tbruton417@gmail.com>
2024-03-17 22:26:41 -0400

    Finished

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,32 +1,56 @@
 package io.zipcoder.polymorphism;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MainApplication {
 
-    LinkedHashMap<String, String> getPets(){
+    public static void main(String[] args){
+        MainApplication main = new MainApplication();
+        LinkedHashMap<Pet, String> pets = main.getPets();
+        printPetsAndSpeak(pets);
+    }
+
+
+
+
+    LinkedHashMap<Pet, String> getPets() {
         Scanner newScanner = new Scanner(System.in);
         System.out.println("How many pets do you have");
         int numPets = newScanner.nextInt();
         newScanner.nextLine(); // Get end of line character
-        LinkedHashMap<String, String> pets = new LinkedHashMap<>();
+        LinkedHashMap<Pet, String> pets = new LinkedHashMap<>();
 
 
-        for(int i = 0; i < numPets; i++){
+        for (int i = 0; i < numPets; i++) {
             System.out.println("Enter type of pet");
             String petType = newScanner.nextLine();
 
+
             System.out.println("Enter pet name");
             String petName = newScanner.nextLine();
-            pets.put(petType, petName);
+
+            if (petType.toLowerCase().equals("dog")) {
+                Dog dog = new Dog(petName);
+                pets.put(dog, petName);
+            } else if (petType.toLowerCase().equals("cat")) {
+                Cat cat = new Cat(petName);
+                pets.put(cat, petName);
+            } else if (petType.toLowerCase().equals("macaw")) {
+                Macaw macaw = new Macaw(petName);
+                pets.put(macaw, petName);
+            }
         }
-        System.out.println(pets.toString());
         return pets;
     }
 
-    public static void main(String[] args){
-        MainApplication main = new MainApplication();
-        LinkedHashMap<String, String> pets = main.getPets();
+    public static void printPetsAndSpeak(LinkedHashMap<Pet, String> printPets) {
+        for (Map.Entry<Pet, String> entry : printPets.entrySet()) {
+            System.out.println(entry.getKey().getName());
+            System.out.println(entry.getKey().speak());
+        }
     }
+
+
 }
